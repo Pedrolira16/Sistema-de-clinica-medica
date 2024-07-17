5f91bc0a2d7701a8557d574b51bbc12e7e4642ca
Pedro Lira <pedrolira.2004@gmail.com>
2024-07-17 10:45:20 -0300

    .

---
+++ src/routes/patient.js
@@ -0,0 +1,8 @@
+import BaseRoutes from "./base";
+import SchemaValidator from "../utils/schemaValidator";
+import patientSchema from "../schemas/patient";
+import PatientController from "../controller/patient";
+ 
+class PatientRoutes extends BaseRoutes {
+	
+}
