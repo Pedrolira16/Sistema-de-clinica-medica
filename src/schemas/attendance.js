91ba9d6dd3ea0326aaf0f397307e04a7ed51576a
Pedro Lira <pedrolira.2004@gmail.com>
2024-07-24 21:38:09 -0300

    update attendance

--- src/schemas/attendance.js
+++ src/schemas/attendance.js
@@ -2,17 +2,22 @@ import moment from 'moment';
 import * as yup from 'yup';
 
 const attendanceSchema = {
-	create: {
-		body: yup
-			.object({
-				user_id: yup.number().required(),
-				place_id: yup.number().required(),
-				patient_id: yup.number().required(),
-				start_date: yup.string().test('invalidFormat', null, value => !value || (moment(value, 'YYYY-MM-DD, HH:mm', true).isValid() && moment(value).isAfter(moment()))).nullable(),
-				end_date: yup.string().test('invalidFormat', null, value => !value || (moment(value, 'YYYY-MM-DD, HH:mm', true).isValid() && moment(value).isAfter(moment(start_date)))).nullable()
-			})
-			.unknown()
-	},
+    create: {
+        body: yup
+            .object({
+                user_id: yup.number().required(),
+                place_id: yup.number().required(),
+                patient_id: yup.number().required(),
+                start_date: yup.string().test('invalidFormat', null, function(value) {
+                    return !value || (moment(value, 'YYYY-MM-DD', true).isValid() && moment(value).isAfter(moment()));
+                }).nullable(),
+                end_date: yup.string().test('invalidFormat', null, function(value) {
+                    const { start_date } = this.parent;
+                    return !value || (moment(value, 'YYYY-MM-DD', true).isValid() && moment(value).isAfter(moment(start_date)));
+                }).nullable(),
+            })
+            .unknown()
+    },
 
 	list: {
 		query: yup
